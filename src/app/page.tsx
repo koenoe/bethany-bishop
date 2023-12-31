9dd1d7cc809c64e9f5090b2df67bc122299bc8f4
Koen Romers <koenoe@users.noreply.github.com>
2023-12-31 14:05:18 +0000

    chore: empty meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from 'next';
 import Hero from '@/components/Hero';
+
+export const metadata: Metadata = {
+  description: '',
+};
 
 export default function Home() {
   return <Hero />;
