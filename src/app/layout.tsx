25334fe7f8e092ab7f9bce06b26196852cf38550
Koen Romers <koenoe@users.noreply.github.com>
2023-12-31 11:32:16 +0000

    chore: noindex,nofollow for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="robots" content="noindex,nofollow" />
       <body className={inter.className}>{children}</body>
     </html>
   )
