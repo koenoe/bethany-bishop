fb76043a6b4bac46a35d61cc67550eb9d7d4c1e8
Koen Romers <koenoe@users.noreply.github.com>
2023-12-31 13:01:23 +0000

    chore: add fonts and logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: [
@@ -8,10 +9,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-questrial)', ...defaultTheme.fontFamily.sans],
+        logo: 'var(--font-katibeh)',
+      },
+      colors: {
+        primary: '#fd883d',
+        secondary: '#6a86d8',
       },
     },
   },
