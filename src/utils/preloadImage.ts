588385513c1cd8131f0c4373db0f6b1936a1ea6e
Koen Romers <koenoe@users.noreply.github.com>
2024-01-14 00:53:01 +0000

    chore: add limit to image cache

--- src/utils/preloadImage.ts
+++ src/utils/preloadImage.ts
@@ -1,4 +1,12 @@
+const MAX_CACHE_SIZE = 1000;
 const cache = new Map<string, HTMLImageElement>();
+
+function evictCache() {
+  while (cache.size > MAX_CACHE_SIZE) {
+    const firstKey = cache.keys().next().value;
+    cache.delete(firstKey);
+  }
+}
 
 export default function preloadImage(src: string) {
   return new Promise((resolve, reject) => {
@@ -10,6 +18,9 @@ export default function preloadImage(src: string) {
     const img = new Image();
     img.src = src;
     img.onload = () => {
+      if (cache.size >= MAX_CACHE_SIZE) {
+        evictCache();
+      }
       cache.set(src, img);
       resolve(img);
     };
